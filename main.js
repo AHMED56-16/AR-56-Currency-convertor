6f8de7030d19e0d9143945f0734e0667e34e425a
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-06 07:13:17 +0500

    Currency convertor

---
+++ main.js
@@ -0,0 +1,50 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+const currencies = [
+    { name: 'USD', value: 1 },
+    { name: 'EUR', value: 0.91 },
+    { name: 'GBP', value: 0.76 },
+    { name: 'INR', value: 74.57 },
+    { name: 'PKR', value: 280 }
+];
+console.log(chalk.bgBlueBright('Welcome to the Currency Converter!'));
+(async () => {
+    while (true) {
+        let userAns = await inquirer.prompt([
+            {
+                name: 'from',
+                type: 'list',
+                message: chalk.bgYellowBright('Enter From Currency?'),
+                choices: currencies.map(currency => currency.name)
+            },
+            {
+                name: 'to',
+                type: 'list',
+                message: chalk.bgCyanBright('Enter To Currency?'),
+                choices: currencies.map(currency => currency.name)
+            },
+            {
+                name: 'amount',
+                type: 'number',
+                message: chalk.redBright('Enter Your desired Amount you want to convert?'),
+            }
+        ]);
+        let fromCurrency = currencies.find(currency => currency.name === userAns.from);
+        let toCurrency = currencies.find(currency => currency.name === userAns.to);
+        let amount = userAns.amount;
+        let convertedAmount = amount / fromCurrency.value * toCurrency.value;
+        console.log(chalk.green(`Converted Amount: ${convertedAmount.toFixed(2)} ${userAns.to}`));
+        let confirm = await inquirer.prompt([
+            {
+                name: 'toprocess',
+                type: "list",
+                message: chalk.gray("You want to Continue or Exit?"),
+                choices: ['Continue', 'Exit']
+            }
+        ]);
+        if (confirm.toprocess === 'Exit') {
+            console.log(chalk.green("Thanks For using CLI Currency Converter"));
+            break;
+        }
+    }
+})();
